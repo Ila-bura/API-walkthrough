71be1a36622192c72b2a7a780293c803763f1139
Ila-bura <>
2023-08-19 08:09:07 +0000

    Add constants

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,3 @@
-EUDJ8ch8ZntBjRH-bKHZEb-Zv5M
+const API_KEY = "EUDJ8ch8ZntBjRH-bKHZEb-Zv5M";
+const API_URL = "https://ci-jshint.herokuapp.com/api";
+const resultsModal = new Bootstrap.Modal(document.getElementById("resultsModal"));
