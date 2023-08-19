4fe1ce49cab18680cbf7d1a0ce902a42971fac98
Ila-bura <>
2023-08-19 10:25:52 +0000

    Fix subtle error

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,29 +6,67 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
+function processOptions(form) {
+    let optArray = [];
+
+    for (let e of form.entries()) {
+        if (e[0] === "options") {
+            optArray.push(e[1]);
+        }
+    }
+
+    form.delete("options");
+
+    form.append("options", optArray.join());
+
+    return form;
+}
+
 async function postForm(e) {
-    const form = new FormData(document.getElementById("checksform"));
+
+    const form = processOptions(new FormData(document.getElementById("checksform")));
+
     const response = await fetch(API_URL, {
-                        method: "POST",
-                        headers: {
-                                    "Authorization": API_KEY,
-                                 },
-                                 body: form,
-                        });
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form,
+    });
+
     const data = await response.json();
-    if (response.ok)       {
+
+    if (response.ok) {
         displayErrors(data);
     } else {
-        throw new Error (data.error);
-    }           
-   
+        throw new Error(data.error);
+    }
+
+}
+
+async function getStatus(e) {
+
+    const queryString = `${API_URL}?api_key=${API_KEY}`;
+
+    const response = await fetch(queryString);
+
+    const data = await response.json();
+
+    if (response.ok) {
+        displayStatus(data);
+    } else {
+        throw new Error(data.error);
+    }
+
 }
 
 function displayErrors(data) {
-    let heading = 'JSHint Results for ${data.file}';
 
+    let results = "";
+
+    let heading = `JSHint Results for ${data.file}`;
     if (data.total_errors === 0) {
-        results = '<div class = "no_errors">No errors reported!</div>';
+        results = `<div class="no_errors">No errors reported!</div>`;
     } else {
         results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
         for (let error of data.error_list) {
@@ -37,35 +75,14 @@ function displayErrors(data) {
             results += `<div class="error">${error.error}</div>`;
         }
     }
+
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
     resultsModal.show();
 }
 
-
-//Wire up the button with event listener//
-//Arrow function is used as even handler//
-//Takes one parameter, e, which represents the event object//
-
-document.getElementById("status").addEventListener("click", e => getStatus(e));
-
-async function getStatus(e) {
-
-    const queryString = `${API_URL}?api_key=${API_KEY}`;
-
-    const response = await fetch(queryString);
-        //When the response comes back, convert to json//
-
-    const data = await response.json();
-
-    if (response.ok) {
-        displayStatus(data);
-    }
-        else {
-            throw new Error(data.error);
-        }
-    }
 function displayStatus(data) {
+
     let heading = "API Key Status";
     let results = `<div>Your key is valid until</div>`;
     results += `<div class="key-status">${data.expiry}</div>`;
@@ -73,6 +90,5 @@ function displayStatus(data) {
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
     resultsModal.show();
+
 }
-
-
